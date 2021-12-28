69fad289085c386c7f02a50b353c37c5c6d155bf
Laknath1996 <ashwindesilva@yahoo.com>
2021-12-28 11:33:55 -0500

    Add results & figs from server

--- benchmarks/kdn_experiments/run_experiments.sh
+++ benchmarks/kdn_experiments/run_experiments.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 ## Gaussian Parity
-python gaussian_parity_exp.py -c 1 -k 1e-5 -reps 10
+python gaussian_parity_exp.py -c 1 -k 1e-5 -reps 100
 
 # ## Spiral
-# python spiral_exp.py -c 3 -k 1e-5 -reps 10
+python spiral_exp.py -c 3 -k 1e-5 -reps 100
 
 # ## Ellipse
-# python ellipse_exp.py -c 3 -k 1e-5 -reps 10
+python ellipse_exp.py -c 3 -k 1e-5 -reps 100
 
 # ## Gaussian Sparse Parity
-# python gaussian_sparse_parity_exp.py -c 2 -k 1e-5 -reps 10
+python gaussian_sparse_parity_exp.py -c 2 -k 1e-5 -reps 100
 
 # ## Step
-# python step_exp.py -c 3 -k 1e-5 -reps 10
+python step_exp.py -c 3 -k 1e-5 -reps 100
 
 # ## Polynomial
-# python polynomial_exp.py -c 2 -k 1e-5 -reps 10
+python polynomial_exp.py -c 2 -k 1e-5 -reps 100
 
 # ## sinewave
-# python sinewave_exp.py -c 2 -k 1e-5 -reps 10
+python sinewave_exp.py -c 2 -k 1e-5 -reps 100
